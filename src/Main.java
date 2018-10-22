722f0d64b457f939cb18d0e3d3d3c6271fb8b52a
Jbladevr <jbladevrmain@yahoo.com>
2018-10-21 21:35:14 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ import coinwheelgame.Wheel;
  */
 public class Main {
     public static void main (String[]args){
-       fourCoinTwoRevealStrategyTest();
+        fourCoinTwoRevealStrategyTest();
     }
 
     public static void fourCoinTwoRevealStrategyTest() {
